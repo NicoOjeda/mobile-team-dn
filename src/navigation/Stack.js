301b01bd273d5cf2ef684d2e713f9a4b75149ed1
Nico ojeda <nico_ojeda86@hotmail.com>
2022-12-09 14:06:25 -0300

    se agrega ruta de shows

--- src/navigation/Stack.js
+++ src/navigation/Stack.js
@@ -3,31 +3,18 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../screens/Home'
 import Hotels from '../screens/Hotels'
 import Hotel from '../screens/Hotel'
+import Shows from '../screens/Shows'
 
 const StackNav = createNativeStackNavigator();
 
 export default function Stack() {
     return (
-        <StackNav.Navigator>
-            <StackNav.Screen name="Home" component={Home} />
+        <StackNav.Navigator screenOptions={{headerShown:false}}>
+            <StackNav.Screen name="Home" component={Home}  />
             <StackNav.Screen name="Hotels" component={Hotels} />
             <StackNav.Screen name="Hotel Details" component={Hotel} />
+            <StackNav.Screen name="Shows" component={Shows} />
+            <StackNav.Screen name="SignIn" component={SignIn} />
         </StackNav.Navigator>
     )
 }
-
-// import React from 'react';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import Home from '../screens/Home'
-// import Hotels from '../screens/Hotels'
-
-// const StackNav = createNativeStackNavigator();
-
-// export default function Stack() {
-//     return (
-//         <StackNav.Navigator>
-//             <StackNav.Screen name="Home" component={Home} />
-//             <StackNav.Screen name="Hotels" component={Hotels} />
-//         </StackNav.Navigator>
-//     )
-// }
